01fc3e4298ce84c1a3ecf4891cdcb5db16c13a47
aTang <963749001@qq.com>
2022-06-12 14:40:47 +0800

    [config]add mybatis generator config

--- src/main/java/com/zwt/App.java
+++ src/main/java/com/zwt/App.java
@@ -1,7 +1,12 @@
 package com.zwt;
 
+import com.zwt.dao.UserDOMapper;
+import com.zwt.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
  * Hello world!
  *
  */
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = ("com.zwt"))
 @RestController
-public class App 
-{
+@MapperScan("com.zwt.dao")
+public class App {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String home() {
-        return "home page";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if (userDO == null) {
+            return "用户对象不存在";
+        }else {
+            return userDO.getName();
+        }
     }
 
     public static void main( String[] args ) {
